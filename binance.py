5c1acd30c7f77a5d034fb90fbabc3d027c4f9da0
BlackJackness <alfadiktator@gmail.com>
2018-12-09 13:12:20 +0300

    Graph time changes

--- binance.py
+++ binance.py
@@ -5,11 +5,21 @@ from mpl_finance import candlestick2_ohlc
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+from math import ceil
 
 SEC_PER_DAY = 24 * 3600
+BINANCE_UPLOAD_CONST = 30000
 
-DEFAULT_FROM = datetime.utcfromtimestamp(int(time.time()) - SEC_PER_DAY).strftime('%Y-%m-%d %H:%M:%S')
-DEFAULT_TO = datetime.utcfromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')
+
+def get_timestamp_format(date_from_format, date_to_format, format):
+    if date_from_format is None:
+        date_from_format = datetime.utcfromtimestamp(int(time.time()) - SEC_PER_DAY - time.timezone).strftime(
+            '%Y-%m-%d %H:%M:%S')
+    if date_to_format is None:
+        date_to_format = datetime.utcfromtimestamp(int(time.time()) - time.timezone).strftime('%Y-%m-%d %H:%M:%S')
+    date_from = int(time.mktime(datetime.strptime(date_from_format, format).timetuple())) + time.timezone
+    date_to = int(time.mktime(datetime.strptime(date_to_format, format).timetuple())) + time.timezone
+    return date_from, date_to, date_from_format, date_to_format
 
 
 class BinanceExchange:
@@ -21,19 +31,23 @@ class BinanceExchange:
         return f'''Текущий курс заданной пары {sell_coin}/{buy_coin} : {rate}
     Значит,при продаже {number} {sell_coin} вы получите {float(rate) * float(number)} {buy_coin}'''
 
-    def print_graph(self, sell_coin, buy_coin='USDT', date_from_format=DEFAULT_FROM, date_to_format=DEFAULT_TO,
+    def print_graph(self, sell_coin, buy_coin='USDT', date_from_format=None, date_to_format=None,
                     format='%Y-%m-%d %H:%M:%S'):
-        date_from = int(time.mktime(datetime.strptime(date_from_format, format).timetuple()))
-        date_to = int(time.mktime(datetime.strptime(date_to_format, format).timetuple()))
-        r = requests.get(
-            f'https://www.binance.com/api/v1/klines?symbol={sell_coin}{buy_coin}&interval=1m&endTime={date_to * 1000}')
+        date_from, date_to, date_from_format, date_to_format = get_timestamp_format(date_from_format, date_to_format,
+                                                                                    format)
+        number_of_upload = ceil((date_to - date_from) / BINANCE_UPLOAD_CONST)
+        candles = []
+        for interval in range(1, number_of_upload + 1):
+            r = requests.get(
+                f'https://www.binance.com/api/v1/klines?symbol={sell_coin}{buy_coin}&interval=1m&endTime={(date_from + BINANCE_UPLOAD_CONST*interval) * 1000}')
+            candles += r.json()
         opened = []
         high = []
         low = []
         close = []
         xdate = []
-        for item in r.json():
-            candle_time = int(item[0]) // 1000
+        for item in candles:
+            candle_time = int(item[0]) // 1000 - time.timezone
             if candle_time < date_from:
                 continue
             opened.append(float(item[1]))
@@ -62,14 +76,14 @@ class BinanceExchange:
         hash_name = hex(
             abs(hash('bin')) * abs(hash(sell_coin) + hash(buy_coin) * hash(date_from_format) + hash(date_to_format)))
         plt.savefig(f'{hash_name}.png')
-        return f'./{hash_name}.png'
+        return f'{hash_name}.png'
 
     def last_trades(self, sell_coin, buy_coin='USDT', limit=20):
         r = requests.get(f'https://www.binance.com/api/v1/aggTrades?limit={limit}&symbol={sell_coin}{buy_coin}')
         last_trades_list = [f'Последние сделки в паре {sell_coin}/{buy_coin}']
         ind = 1
         for item in r.json():
-            date = datetime.utcfromtimestamp(int(item['T']) // 1000).strftime('%Y-%m-%d %H:%M:%S')
+            date = datetime.utcfromtimestamp(int(item['T']) // 1000 - time.timezone).strftime('%Y-%m-%d %H:%M:%S')
             quantity, price = item['q'], item['p']
             buy_volume = round(float(price) * float(quantity), 6)
             last_trades_list += [
@@ -77,13 +91,15 @@ class BinanceExchange:
             ind += 1
         return "\n".join(last_trades_list)
 
-    def do_analytics(self, sell_coin, buy_coin='USDT', date_from_format=DEFAULT_FROM, date_to_format=DEFAULT_TO,
+    def do_analytics(self, sell_coin, buy_coin='USDT', date_from_format=None, date_to_format=None,
                      format='%Y-%m-%d %H:%M:%S'):
-        date_from = int(time.mktime(datetime.strptime(date_from_format, format).timetuple()))
-        date_to = int(time.mktime(datetime.strptime(date_to_format, format).timetuple()))
+        date_from, date_to, date_from_format, date_to_format = get_timestamp_format(date_from_format, date_to_format,
+                                                                                    format)
+        r = requests.get(
+            f'https://www.binance.com/api/v1/klines?symbol={sell_coin}{buy_coin}&interval=1m&endTime={date_from * 1000}')
+        open_cost = float(r.json()[-1][4])
         r = requests.get(
             f'https://www.binance.com/api/v1/klines?symbol={sell_coin}{buy_coin}&interval=1m&endTime={date_to * 1000}')
-        open_cost = float(r.json()[0][1])
         close_cost = float(r.json()[-1][4])
         delta_cost = round(close_cost - open_cost, 9)
         return f'''Стоймость {sell_coin} при открытии {date_from_format} : {open_cost} {buy_coin}
--- exmo.py
+++ exmo.py
@@ -11,8 +11,16 @@ import numpy as np
 
 SEC_PER_DAY = 24 * 3600
 
-DEFAULT_FROM = datetime.utcfromtimestamp(int(time.time()) - SEC_PER_DAY).strftime('%Y-%m-%d %H:%M:%S')
-DEFAULT_TO = datetime.utcfromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')
+
+def get_timestamp_format(date_from_format, date_to_format, format):
+    if date_from_format is None:
+        date_from_format = datetime.utcfromtimestamp(int(time.time()) - SEC_PER_DAY - time.timezone).strftime(
+            '%Y-%m-%d %H:%M:%S')
+    if date_to_format is None:
+        date_to_format = datetime.utcfromtimestamp(int(time.time()) - time.timezone).strftime('%Y-%m-%d %H:%M:%S')
+    date_from = int(time.mktime(datetime.strptime(date_from_format, format).timetuple())) + time.timezone
+    date_to = int(time.mktime(datetime.strptime(date_to_format, format).timetuple())) + time.timezone
+    return date_from, date_to, date_from_format, date_to_format
 
 
 class ExmoExchange:
@@ -52,10 +60,10 @@ class ExmoExchange:
             ind += 1
         return "\n".join(last_trades_list)
 
-    def print_graph(self, sell_coin, buy_coin='USD', date_from_format=DEFAULT_FROM, date_to_format=DEFAULT_TO,
+    def print_graph(self, sell_coin, buy_coin='USD', date_from_format=None, date_to_format=None,
                     format='%Y-%m-%d %H:%M:%S'):
-        date_from = int(time.mktime(datetime.strptime(date_from_format, format).timetuple()))
-        date_to = int(time.mktime(datetime.strptime(date_to_format, format).timetuple()))
+        date_from, date_to, date_from_format, date_to_format = get_timestamp_format(date_from_format, date_to_format,
+                                                                                    format)
         r = requests.get(
             f'https://exmo.me/ctrl/chart/history?symbol={sell_coin}_{buy_coin}&resolution=30&from={date_from}&to={date_to}',
             headers=exmo_headers)
@@ -69,11 +77,12 @@ class ExmoExchange:
             high.append(item['h'])
             low.append(item['l'])
             close.append(item['c'])
-            xdate.append(datetime.fromtimestamp(int(item['t']) // 1000))
+            xdate.append(datetime.fromtimestamp(int(item['t']) // 1000 - time.timezone))
         fig, ax = plt.subplots()
         ax.set_xlabel('Date')
         ax.set_ylabel(f'One {sell_coin} in {buy_coin}')
-        ax.set_title(f'{sell_coin}/{buy_coin} from {date_from_format} to {date_to_format}')
+        ax.set_title(
+            f'{sell_coin}/{buy_coin} from {date_from_format} to {date_to_format}')
         candlestick2_ohlc(ax, np.array(opened), np.array(high), np.array(low), np.array(close), colorup='g', width=0.8)
         ax.xaxis.set_major_locator(ticker.MaxNLocator(6))
 
@@ -91,18 +100,18 @@ class ExmoExchange:
         hash_name = hex(
             abs(hash('bin')) * abs(hash(sell_coin) + hash(buy_coin) * hash(date_from_format) + hash(date_to_format)))
         plt.savefig(f'{hash_name}.png')
-        return f'./{hash_name}.png'
+        return f'{hash_name}.png'
 
-    def do_analytics(self, sell_coin, buy_coin='USD', date_from_format=DEFAULT_FROM, date_to_format=DEFAULT_TO,
+    def do_analytics(self, sell_coin, buy_coin='USD', date_from_format=None, date_to_format=None,
                      format='%Y-%m-%d %H:%M:%S'):
-        date_from = int(time.mktime(datetime.strptime(date_from_format, format).timetuple()))
-        date_to = int(time.mktime(datetime.strptime(date_to_format, format).timetuple()))
+        date_from, date_to, date_from_format, date_to_format = get_timestamp_format(date_from_format, date_to_format,
+                                                                                    format)
         r = requests.get(
             f'https://exmo.me/ctrl/chart/history?symbol={sell_coin}_{buy_coin}&resolution=30&from={date_from}&to={date_to}',
             headers=exmo_headers)
         candles = r.json()['candles']
-        open_cost = float(r.json()['candles'][0]['o'])
-        close_cost = float(r.json()['candles'][-1]['c'])
+        open_cost = float(candles[0]['o'])
+        close_cost = float(candles[-1]['c'])
         delta_cost = round(close_cost - open_cost, 9)
         return f'''Стоймость {sell_coin} при открытии {date_from_format} : {open_cost} {buy_coin}
     Стоймость {sell_coin} при закрытии {date_to_format} : {close_cost} {buy_coin}
